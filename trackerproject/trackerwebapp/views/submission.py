a247793af0e174bea7c93ac534d572811c1385ba
Steve Brownlee <chortlehoort@gmail.com>
2020-07-02 21:58:13 +0000

    Only record exercise if it hasn't been previously completed

--- trackerproject/trackerwebapp/views/submission.py
+++ trackerproject/trackerwebapp/views/submission.py
@@ -8,6 +8,7 @@ from ..models import Student, Classroom, Submission
 @csrf_exempt
 def submission(request):
     sub = json.loads(request.body.decode("utf-8"))
+    print(sub)
 
     try:
         student = Student.objects.get(replit_id=sub['student']['id'])
@@ -27,13 +28,16 @@ def submission(request):
         classroom.save()
 
     if sub['submission']['status'] == "complete":
-        submission = Submission()
-        submission.time_submitted = sub['submission']['time_submitted']
-        submission.teacher_url = sub['submission']['teacher_url']
-        submission.student_url = sub['submission']['student_url']
-        submission.exercise = sub['assignment']['name']
-        submission.student = student
-        submission.classroom = classroom
-        submission.save()
+        try:
+            already_recorded = Submission.objects.get(student=student, classroom=classroom, exercise=sub['assignment']['name'])
+        except Submission.DoesNotExist:
+            submission = Submission()
+            submission.time_submitted = sub['submission']['time_submitted']
+            submission.teacher_url = sub['submission']['teacher_url']
+            submission.student_url = sub['submission']['student_url']
+            submission.exercise = sub['assignment']['name']
+            submission.student = student
+            submission.classroom = classroom
+            submission.save()
 
     return HttpResponse('Thank you')
