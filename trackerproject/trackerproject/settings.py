ac27451c7e6bd52833112a9eedebee6d4367ff76
Steve Brownlee <chortlehoort@gmail.com>
2020-07-03 19:20:10 +0000

    Using static directive

--- trackerproject/trackerproject/settings.py
+++ trackerproject/trackerproject/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/usr/share/nginx/html/tracker'
+STATIC_ROOT = '/var/www/tracker/static'
